a5131eac230e4a78040b26ca0d8b333384ff0af9
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-21 01:08:24 +0100

    menambah fitur input user

--- tugasInterpol_StrukturData_Dimas_20Mei2026.py
+++ tugasInterpol_StrukturData_Dimas_20Mei2026.py
@@ -1,6 +1,12 @@
-listdata = [7,20,25,36,42,51,64,76,83,99,100]
-print(listdata)
+#listdata = [7,20,25,36,42,51,64,76,83,99,100]
+import random
 
-rumus = 0 + ((76 - listdata[0])/(listdata[-1]-listdata[0])) * ((len(listdata)-1) - listdata.index(7))
+print("="*8,'\n', "Interpolator",'\n',"="*8)
+demand = int(input('Berapa data yang ingin dibuat? '))
+data_acak = random.sample(range(1,100),demand)
+list_data = sorted(data_acak)
+print (list_data)
+subjek = int(input("Masukkan angka target: "))
+rumus = list_data.index(min(list_data)) + ((subjek - list_data[0])/(list_data[-1]-list_data[0])) * (list_data.index(max(list_data)) - list_data.index(min(list_data)))
 
 print (round(rumus))
